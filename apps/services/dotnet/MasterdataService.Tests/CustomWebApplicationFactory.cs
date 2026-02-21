e1b208f987adc1d030165d36aa1110e0a6f310d0
JCTRoth <jonasroth@mailbase.info>
2026-02-21 20:55:07 +0100

    Fix CI test failures: Add environment guards for database init in tests, stabilize Masterdata assertions

--- apps/services/dotnet/MasterdataService.Tests/CustomWebApplicationFactory.cs
+++ apps/services/dotnet/MasterdataService.Tests/CustomWebApplicationFactory.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using System.Collections.Generic;
 using MasterdataService.Data;
 
 namespace MasterdataService.Tests;
@@ -10,7 +11,14 @@ public class CustomWebApplicationFactory : WebApplicationFactory<Program>
 {
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
-        builder.UseEnvironment("Development");
+        builder.UseEnvironment("Testing");
+        builder.ConfigureAppConfiguration(config =>
+        {
+            config.AddInMemoryCollection(new Dictionary<string, string>
+            {
+                ["SkipDatabaseInitialization"] = "true"
+            });
+        });
         builder.ConfigureServices(services =>
         {
             var descriptor = services.SingleOrDefault(
