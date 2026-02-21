7d79cdb3b414ff70f0deed5c638b2895bac0c3b4
JCTRoth <jonasroth@mailbase.info>
2026-02-21 20:59:20 +0100

    Fix missing using directive for AddInMemoryCollection in test factories

--- apps/services/dotnet/ShopService.Tests/Controllers/ProductsControllerTests.cs
+++ apps/services/dotnet/ShopService.Tests/Controllers/ProductsControllerTests.cs
@@ -4,6 +4,7 @@ using FluentAssertions;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using ShopService.Data;
 using ShopService.DTOs;
